e644e2949e677fd1fef490e652584e4d7f79db98
Martin Evans <martindevans@gmail.com>
2011-02-28 00:31:00 +0000

    Added tge three basic halfedge datastructure classes Implemented some helper methods on Face

---
+++ GeometrySharp/HalfEdgeGeometry/Face.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometrySharp.HalfEdgeGeometry
+{
+    public class Face
+    {
+        public HalfEdge Edge;
+
+        public IEnumerable<HalfEdge> Edges
+        {
+            get
+            {
+                HalfEdge e = Edge;
+
+                do
+                {
+                    yield return e;
+                    e = e.Next;
+                } while (e != null && e != Edge);
+            }
+        }
+
+        public IEnumerable<Vertex> Vertices
+        {
+            get
+            {
+                foreach (var edge in Edges)
+                    yield return edge.End;
+            }
+        }
+
+        public IEnumerable<Face> Neighbours
+        {
+            get
+            {
+                foreach (var edge in Edges)
+                    yield return edge.Twin == null ? null : edge.Twin.Face;
+            }
+        }
+    }
+}
---
+++ GeometrySharp/HalfEdgeGeometry/HalfEdge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometrySharp.HalfEdgeGeometry
+{
+    public class HalfEdge
+    {
+        public Vertex End;
+        public HalfEdge Twin;
+        public HalfEdge Next;
+        public Face Face;
+    }
+}
