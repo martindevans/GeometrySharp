8281d400311335ac6dc172f5c4e0bcd0d8aab98b
Martin Evans <martindevans@gmail.com>
2011-03-02 02:32:52 +0000

    Added MeshTest which I forgot in the last commit Updated gitignore to ignore TestResults
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ GeometrySharpUnitTests/MeshTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometrySharp.HalfEdgeGeometry;
+using Microsoft.Xna.Framework;
+
+namespace GeometrySharpUnitTests
+{
+    [TestClass]
+    public class MeshTest
+    {
+        #region vertices
+        [TestMethod]
+        public void GetTwoDifferentVertices()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+            Assert.AreEqual(Vector3.Zero, a.Position);
+
+            var b = m.GetVertex(new Vector3(1, 2, 3));
+            Assert.AreEqual(new Vector3(1, 2, 3), b.Position);
+
+            Assert.AreNotEqual(a, b);
+        }
+
+        [TestMethod]
+        public void GetSameVertexTwice()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+            var b = m.GetVertex(Vector3.Zero);
+            var c = m.GetVertex(Vector3.One);
+            var d = m.GetVertex(Vector3.One);
+
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(c, d);
+            Assert.AreNotEqual(a, c);
+        }
+
+        [TestMethod]
+        public void GetSimilarVertices()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(new Vector3(0, 0, 0));
+            var b = m.GetVertex(new Vector3(Mesh.BUCKET_SIZE / 2, 0, 0));
+
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(a.Position, Vector3.Zero);
+            Assert.AreEqual(b.Position, Vector3.Zero);
+        }
+        #endregion
+
+        #region edges
+        [TestMethod]
+        public void GetTwoDifferentEdges()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+            var b = m.GetVertex(Vector3.One);
+            var c = m.GetVertex(Vector3.UnitX);
+
+            var e1 = m.GetEdge(a, b);
+            var e2 = m.GetEdge(b, c);
+
+            Assert.AreNotEqual(e1, e2);
+        }
+
+        [TestMethod]
+        public void GetSameEdgeTwice()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+            var b = m.GetVertex(Vector3.One);
+
+            var e1 = m.GetEdge(a, b);
+            var e2 = m.GetEdge(a, b);
+
+            Assert.AreEqual(e1, e2);
+        }
+
+        [TestMethod]
+        public void GetEdgeAndTwinSeparately()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+            var b = m.GetVertex(Vector3.One);
+
+            var e1 = m.GetEdge(a, b);
+            var e2 = m.GetEdge(b, a);
+
+            Assert.AreEqual(e1, e2.Twin);
+            Assert.AreEqual(e1.Twin, e2);
+        }
+        #endregion
+
+        #region faces
+        [TestMethod]
+        public void GetFace()
+        {
+            Mesh m = new Mesh();
+            
+            var a = m.GetVertex(Vector3.UnitY);
+            var b = m.GetVertex(Vector3.UnitX);
+            var c = m.GetVertex(-Vector3.UnitX);
+
+            var f = m.GetFace(a, b, c);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetFaceWithDuplicateVertices()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+
+            m.GetFace(a, a, a);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void GetFaceWithConflictingEdgeAlreadyInPlace()
+        {
+            Mesh m = new Mesh();
+
+            var a = m.GetVertex(Vector3.Zero);
+            var b = m.GetVertex(Vector3.UnitX);
+            var c = m.GetVertex(Vector3.UnitY);
+            var d = m.GetVertex(Vector3.UnitZ);
+
+            var f = m.GetFace(a, b, c);
+
+            var f2 = m.GetFace(a, b, d);
+        }
+        #endregion
+    }
+}
